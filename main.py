78b2b45585af0d036dba8be64478ceac3eac8bf1
kj12354 <147910458+kj12354@users.noreply.github.com>
2023-10-30 11:37:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import cv2
 import pytesseract
 from gtts import gTTS
 import os
-from scapy.all import sniff, ARP
+import spacy
+
+# Load the spaCy English model
+nlp = spacy.load("en_core_web_sm")
+
 
 # Function to extract text from a video frame
 def extract_text_from_frame(frame):
@@ -10,15 +14,23 @@ def extract_text_from_frame(frame):
     text = pytesseract.image_to_string(frame)
     return text
 
-# Function to convert text to speech
+
+# Function to process and convert text to speech
 def text_to_speech(text):
-    tts = gTTS(text, lang='en')  # Adjust the language as needed
+    # Process text with spaCy
+    doc = nlp(text)
+
+    # Extract and join only the recognized entities
+    entities = " ".join(entity.text for entity in doc.ents)
+
+    tts = gTTS(entities, lang='en')
     tts.save('output.mp3')
     os.system('start output.mp3')  # Adjust for your operating system's audio player
 
+
 if __name__ == "__main__":
-    # Initialize the video capture from a camera (you can specify the camera index)
-    capture = cv2.VideoCapture(0)  # 0 corresponds to the default camera
+    # Initialize the video capture from a camera
+    capture = cv2.VideoCapture(0)
 
     while True:
         ret, frame = capture.read()  # Read a frame from the camera
@@ -37,18 +49,19 @@ if __name__ == "__main__":
         # Display the video feed
         cv2.imshow('Text Reader', frame)
 
-        # Press 'q' to exit the program
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if not cv2.waitKey(1) & 0xFF != ord('q'):
             break
 
     # Release the video capture and close all OpenCV windows
     capture.release()
     cv2.destroyAllWindows()
 
+
 # Scapy Packet Sniffing Function
 def packet_sniffer(pkt):
     if ARP in pkt:
         print("ARP Packet Detected: ", pkt.summary())
+
 
 # Start packet sniffing
 sniff(filter="arp", prn=packet_sniffer)
